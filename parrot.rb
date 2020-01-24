d63083f14e9fca9f8ce183c69c25769d270bcd47
Andres Carvajal <jc1993box@hotmail.com>
2020-01-24 05:36:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (string)
-  puts "#{string}"
+def parrot (string="Squak")
+  puts "#{string}!"
   string
 end
